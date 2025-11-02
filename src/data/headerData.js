98e3fab2986f909bacfced713a82590209a1e364
himams1 <himahimams4@gmail.com>
2025-11-02 11:58:12 +0530

    image error solved

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
     title: "Hello! I am",
     desciption: "Hello! I’m HIMA M S, a passionate full-stack developer with a strong focus on continuous learning and problem-solving. I thrive in dynamic environments where I can face new challenges and apply innovative solutions.Driven by curiosity, I’m always eager to learn new technologies and expand my skill set. I believe in creating meaningful, high-impact projects, and I am dedicated to delivering results through efficient, scalable, and well-structured solutions.",
     image: dp,
-    imagebw: 'c:\Users\User\Documents\WhatsApp Image 2025-10-31 at 14.15.01_eb8bb34c.jpg',
+    // imagebw: 'hima.jpg',
     resumePdf: 'https://drive.google.com/file/d/1KBnP7jRAL9zk1oBqOuqtOVT2-3kR0rdQ/view?usp=drivesdk',
 }
